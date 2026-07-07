63fa8df9bc296e722ea0ab75deb20cb32b1f87b8
prishajha <prishajha1@gmail.com>
2026-07-07 11:43:05 -0500

    Initial Commit

---
+++ capstone/capstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  capstoneApp.swift
+//  capstone
+//
+//  Created by Prisha Jha on 7/7/26.
+//
+
+import SwiftUI
+
+@main
+struct capstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
